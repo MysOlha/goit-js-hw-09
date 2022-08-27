d95654a9c8bee52efb96d3aa24ea6b44bde59496
MysOlha <misarosh70@gmail.com>
2022-08-27 22:58:23 +0300

    1-2 add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+btnStart.addEventListener('click', onBtnStart)
+btnStop.addEventListener('click', onBtnStop)
+
+let timerId = 0
+
+function onBtnStart() {
+    timerId = setInterval(()=>{
+        body.style.backgroundColor = getRandomHexColor()  
+    }, 1000);
+     btnStart.setAttribute('disabled', true) 
+}
+
+function onBtnStop(){
+    clearInterval(timerId)
+    btnStart.removeAttribute('disabled') 
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css";
+
+const btnStart = document.querySelector('button[data-start]')
+btnStart.setAttribute('disabled', true) 
+const daysWindow = document.querySelector('span[data-days]')
+const hoursWindow = document.querySelector('span[data-hours]')
+const minutesWindow = document.querySelector('span[data-minutes]')
+const secondsWindow = document.querySelector('span[data-seconds]')
+
+
+const input = document.querySelector("#datetime-picker");
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+    
+
+      if((selectedDates[0]) < Date.now()) {
+      window.alert("Please choose a date in the future")
+      return
+    }
+    console.log(selectedDates[0])
+     
+    btnStart.removeAttribute('disabled') 
+
+    btnStart.addEventListener('click', () => {
+        timerId = setInterval(() => {
+
+            btnStart.setAttribute('disabled', true) 
+            
+            now = new Date().getTime()
+            let ms = (selectedDates[0]) - now
+                        
+            const { days, hours, minutes, seconds } = convertMs(ms)
+            console.log(`${days}:${hours}:${minutes}:${seconds}`)
+            if(ms < 1000){
+            clearInterval(timerId) 
+            }     
+            
+        daysWindow.textContent = `${days}`;
+        hoursWindow.textContent = `${hours}`;
+        minutesWindow.textContent = `${minutes}`;
+        secondsWindow.textContent = `${seconds}`;
+            
+        }, 1000)
+    })
+
+} 
+  }
+
+
+flatpickr(input, options)
+
+
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+
+
+function convertMs(ms) {  
+    
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+    return { days, hours, minutes, seconds };
+  }
+  
